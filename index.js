944697771b45d8669c63a79fd6d914acf2ea681d
Jan Sonntag <jan.sonntag@hsrw.org>
2021-01-05 17:12:13 +0100

    SetTimeout

--- index.js
+++ index.js
@@ -68,63 +68,64 @@ async function startProcess() {
         visible: true
     });
     console.log("[8/12] Measurement loaded");
-
-    // Get all the table rows - Variables of one Measurement
-    const variableRows = await page.evaluate(() => {
-        let headTMP = Array.from(document.querySelectorAll('.ui-dialog thead th'));
-        headTMP = headTMP.map(td => td.innerHTML);
-        let head = [];
-        // Cleanup Head
-        headTMP.forEach(element => {
-            // Make an array of all the headers
-            if (!element.includes("</div>")) {
-                head.push(element);
-            }
+    setTimeout(async () => {
+        // Get all the table rows - Variables of one Measurement
+        const variableRows = await page.evaluate(() => {
+            let headTMP = Array.from(document.querySelectorAll('.ui-dialog thead th'));
+            headTMP = headTMP.map(td => td.innerHTML);
+            let head = [];
+            // Cleanup Head
+            headTMP.forEach(element => {
+                // Make an array of all the headers
+                if (!element.includes("</div>")) {
+                    head.push(element);
+                }
+            });
+            // All rows without header
+            let rows = Array.from(document.querySelectorAll('.ui-dialog tbody tr'));
+            return {
+                "head": head,
+                "body": rows.map(td => td.innerHTML)
+            };
         });
-        // All rows without header
-        let rows = Array.from(document.querySelectorAll('.ui-dialog tbody tr'));
-        return {
-            "head": head,
-            "body": rows.map(td => td.innerHTML)
-        };
-    });
-    console.log("[9/12] All variable of the random measurement loaded");
-    console.log("[9/12] raw:", variableRows);
-    console.log("[10/12] Converting to table")
+        console.log("[9/12] All variable of the random measurement loaded");
+        console.log("[9/12] raw:", variableRows);
+        console.log("[10/12] Converting to table")
 
-    // Convert the HTML to just the actual Values
-    // Idea: Maybe it is possible to get only the InnerHTML diretly in variableRows, but this does work for a proof-of-concept
-    var convertedRows = [];
-    variableRows.body.forEach(row => {
-        tmpRow = {};
-        // Split the individual columns of that row
-        row = row.split("\n");
-        // Iterate through all these columns
-        row.forEach((element, i) => {
-            // Find the value in the HTML
-            if (element.match(new RegExp(">.*<")) != null) {
-                tmpRow[variableRows.head[i]] = element.match(new RegExp(">.*<"))[0];
-                // Delete the outer < >
-                tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].substring(1);
-                tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].slice(0, tmpRow[variableRows.head[i]].length - 1);
-                // Delete if div are still there
-                if (tmpRow[variableRows.head[i]].includes("<div")) {
-                    tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].match(new RegExp(">.*<"))[0];
+        // Convert the HTML to just the actual Values
+        // Idea: Maybe it is possible to get only the InnerHTML diretly in variableRows, but this does work for a proof-of-concept
+        var convertedRows = [];
+        variableRows.body.forEach(row => {
+            tmpRow = {};
+            // Split the individual columns of that row
+            row = row.split("\n");
+            // Iterate through all these columns
+            row.forEach((element, i) => {
+                // Find the value in the HTML
+                if (element.match(new RegExp(">.*<")) != null) {
+                    tmpRow[variableRows.head[i]] = element.match(new RegExp(">.*<"))[0];
                     // Delete the outer < >
                     tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].substring(1);
                     tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].slice(0, tmpRow[variableRows.head[i]].length - 1);
+                    // Delete if div are still there
+                    if (tmpRow[variableRows.head[i]].includes("<div")) {
+                        tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].match(new RegExp(">.*<"))[0];
+                        // Delete the outer < >
+                        tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].substring(1);
+                        tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].slice(0, tmpRow[variableRows.head[i]].length - 1);
+                    }
                 }
-            }
+            });
+            convertedRows.push(tmpRow);
         });
-        convertedRows.push(tmpRow);
-    });
 
-    console.log("[11/12] Table conversion complete");
-    console.table(convertedRows, variableRows.head);
-    console.log("[12/12] Job finished!")
+        console.log("[11/12] Table conversion complete");
+        console.table(convertedRows, variableRows.head);
+        console.log("[12/12] Job finished!")
 
-    // Browser schließen
-    await browser.close();
+        // Browser schließen
+        await browser.close();
+    }, 5000);
 }
 
 startProcess();
