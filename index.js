9db29dbb589c2c38eb0a992c74c6f40583d41876
Jan Sonntag <jan.sonntag@hsrw.org>
2021-01-06 12:12:13 +0100

    Update: WaitForSelect: Now waiting for something from the table

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ async function startProcess() {
         return rows.map(td => td.innerHTML);
     });
     console.log("[5/12] Measurement-Rows loaded");
-    console.log('[5/12] measurementRows[50]:', measurementRows[50]);
 
     // Find all measurements to click on
     // To open up all variables that got measured at one measurement a function "getPNAMW(someValue)" has to be executed
@@ -51,11 +50,10 @@ async function startProcess() {
         allMeasurements.push(row.match(new RegExp("getPNAMW[(][0,1,2,3,4,5,6,7,8,9]+[)]"))[0].match(new RegExp("[0,1,2,3,4,5,6,7,8,9]+"))[0]);
     });
     console.log("[6/12]Rows all processed");
-    console.log('[6/12] allMeasurents[50]:', allMeasurements[50]);
 
     // Open up the variable for one random measurement
     // Wait for the site to load and then open up the "Probenliste" (the table with all the samples (but not the actual values))
-    const randomEntry = allMeasurements[50];
+    const randomEntry = allMeasurements[Math.floor(Math.random() * allMeasurements.length - 1)];
     await page.evaluate((randomEntry) => {
         // Slice randomEntry into the individual arguments
         args = randomEntry.split(",");
@@ -64,68 +62,66 @@ async function startProcess() {
     console.log("[7/12] Random Row/Measurement choosen and opened");
 
     // Wait for the list of samples to load
-    await page.waitForSelector(".ui-dialog-title", {
+    await page.waitForSelector(".hygrisc_label", {
         visible: true
     });
     console.log("[8/12] Measurement loaded");
-    setTimeout(async () => {
-        // Get all the table rows - Variables of one Measurement
-        const variableRows = await page.evaluate(() => {
-            let headTMP = Array.from(document.querySelectorAll('.ui-dialog thead th'));
-            headTMP = headTMP.map(td => td.innerHTML);
-            let head = [];
-            // Cleanup Head
-            headTMP.forEach(element => {
-                // Make an array of all the headers
-                if (!element.includes("</div>")) {
-                    head.push(element);
-                }
-            });
-            // All rows without header
-            let rows = Array.from(document.querySelectorAll('.ui-dialog tbody tr'));
-            return {
-                "head": head,
-                "body": rows.map(td => td.innerHTML)
-            };
-        });
-        console.log("[9/12] All variable of the random measurement loaded");
-        console.log("[9/12] raw:", variableRows);
-        console.log("[10/12] Converting to table")
 
-        // Convert the HTML to just the actual Values
-        // Idea: Maybe it is possible to get only the InnerHTML diretly in variableRows, but this does work for a proof-of-concept
-        var convertedRows = [];
-        variableRows.body.forEach(row => {
-            tmpRow = {};
-            // Split the individual columns of that row
-            row = row.split("\n");
-            // Iterate through all these columns
-            row.forEach((element, i) => {
-                // Find the value in the HTML
-                if (element.match(new RegExp(">.*<")) != null) {
-                    tmpRow[variableRows.head[i]] = element.match(new RegExp(">.*<"))[0];
+    // Get all the table rows - Variables of one Measurement
+    const variableRows = await page.evaluate(() => {
+        let headTMP = Array.from(document.querySelectorAll('.ui-dialog thead th'));
+        headTMP = headTMP.map(td => td.innerHTML);
+        let head = [];
+        // Cleanup Head
+        headTMP.forEach(element => {
+            // Make an array of all the headers
+            if (!element.includes("</div>")) {
+                head.push(element);
+            }
+        });
+        // All rows without header
+        let rows = Array.from(document.querySelectorAll('.ui-dialog tbody tr'));
+        return {
+            "head": head,
+            "body": rows.map(td => td.innerHTML)
+        };
+    });
+    console.log("[9/12] All variable of the random measurement loaded");
+    console.log("[10/12] Converting to table")
+
+    // Convert the HTML to just the actual Values
+    // Idea: Maybe it is possible to get only the InnerHTML diretly in variableRows, but this does work for a proof-of-concept
+    var convertedRows = [];
+    variableRows.body.forEach(row => {
+        tmpRow = {};
+        // Split the individual columns of that row
+        row = row.split("\n");
+        // Iterate through all these columns
+        row.forEach((element, i) => {
+            // Find the value in the HTML
+            if (element.match(new RegExp(">.*<")) != null) {
+                tmpRow[variableRows.head[i]] = element.match(new RegExp(">.*<"))[0];
+                // Delete the outer < >
+                tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].substring(1);
+                tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].slice(0, tmpRow[variableRows.head[i]].length - 1);
+                // Delete if div are still there
+                if (tmpRow[variableRows.head[i]].includes("<div")) {
+                    tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].match(new RegExp(">.*<"))[0];
                     // Delete the outer < >
                     tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].substring(1);
                     tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].slice(0, tmpRow[variableRows.head[i]].length - 1);
-                    // Delete if div are still there
-                    if (tmpRow[variableRows.head[i]].includes("<div")) {
-                        tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].match(new RegExp(">.*<"))[0];
-                        // Delete the outer < >
-                        tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].substring(1);
-                        tmpRow[variableRows.head[i]] = tmpRow[variableRows.head[i]].slice(0, tmpRow[variableRows.head[i]].length - 1);
-                    }
                 }
-            });
-            convertedRows.push(tmpRow);
+            }
         });
+        convertedRows.push(tmpRow);
+    });
 
-        console.log("[11/12] Table conversion complete");
-        console.table(convertedRows, variableRows.head);
-        console.log("[12/12] Job finished!")
+    console.log("[11/12] Table conversion complete");
+    console.table(convertedRows, variableRows.head);
+    console.log("[12/12] Job finished!")
 
-        // Browser schließen
-        await browser.close();
-    }, 5000);
+    // Browser schließen
+    await browser.close();
 }
 
 startProcess();
