643fef4ab63eedb57a3cc1c9db1930595a8bba4b
Fabian.Joeken <fabian@adm3.de>
2021-04-01 14:57:08 +0200

    temp

--- extractMeasurementData.js
+++ extractMeasurementData.js
@@ -37,108 +37,118 @@ if (!fs.existsSync(`${process.cwd()}/messstellen`)) {
 
 const spinners = new Spinners();
 
-async function createCSVs(id, showBrowser, from, until) {
-    spinners.add(`spinner-${id}`, { text: `extracting data from measurement point ${id}...`})
-    // Launch a new browser
-    const browser = await puppeteer.launch({
-        headless: !showBrowser,
-        args: ["--disable-setuid-sandbox"],
-        'ignoreHTTPSErrors': true
-    });
-    //adress for details of one measuring point
-    const adress = `https://www.elwasweb.nrw.de/elwas-hygrisc/src/gwmessstelle.php?src=gwmessstelle&tab_index=3&iw=1710&ih=677&block=allgemein&mstnr=${id}`;
-    //create directory for id of measuring point and its data if non existent
-    if (!fs.existsSync(`${process.cwd()}/messstellen/${id}/messdaten`)) {
-        await fs.mkdirSync(`${process.cwd()}/messstellen/${id}`);
-        await fs.mkdirSync(`${process.cwd()}/messstellen/${id}/messdaten`);
-    }
-    //set path to store data, measurement point is identified by its id
-    const measurementPath = path.resolve(
-        process.cwd(),
-        `./messstellen/${id}`,
-    );
-    // Open a new tab and go to the above definied webpage
-    const page = await browser.newPage();
-    try {
-        await page.goto(adress);
-        // Wait for the site to load and then open up the "Probenliste" (the table with all the samples (but not the actual values))
-        await page.evaluate(() => {
-            pfad(this.adress);
-            BlockLaden("probenliste", "#main");
-            // expose function to convert header and row data to JSON
-            window.convertToJSON = function (rows, header, clickValues) {
-                for (let i = 0; i < rows.length; i++) {
-                    let splittedRows = (rows[i].split("\t"));
-                    let objectString = '{';
-                    for (let j = 0; j < header.length; j++) {
-                        //filter for invalid expressions in JSON Data
-                        splittedRows[j] = splittedRows[j].trim();
-                        if (splittedRows[j].includes('"') || splittedRows[j].includes("\\")) {
-                            splittedRows[j] = splittedRows[j].split('"').join();
-                            splittedRows[j] = splittedRows[j].split("\\").join();
+async function createCSVs(points, showBrowser, from, until) {
+    let index = 0;
+    points.forEach(async (point) => {
+        let id = point["LGD-Nummer"]
+        while (index >= 9) {
+            await timeout();
+        }
+        index++;
+        spinners.add(`spinner-${id}`, { text: `extracting data from measurement point ${id}...` })
+        // Launch a new browser
+        const browser = await puppeteer.launch({
+            headless: !showBrowser,
+            args: ["--disable-setuid-sandbox"],
+            'ignoreHTTPSErrors': true
+        });
+        //adress for details of one measuring point
+        const adress = `https://www.elwasweb.nrw.de/elwas-hygrisc/src/gwmessstelle.php?src=gwmessstelle&tab_index=3&iw=1710&ih=677&block=allgemein&mstnr=${id}`;
+        //create directory for id of measuring point and its data if non existent
+        if (!fs.existsSync(`${process.cwd()}/messstellen/${id}/messdaten`)) {
+            await fs.mkdirSync(`${process.cwd()}/messstellen/${id}`);
+            await fs.mkdirSync(`${process.cwd()}/messstellen/${id}/messdaten`);
+        }
+        //set path to store data, measurement point is identified by its id
+        const measurementPath = path.resolve(
+            process.cwd(),
+            `./messstellen/${id}`,
+        );
+        // Open a new tab and go to the above definied webpage
+        const page = await browser.newPage();
+        try {
+            await page.goto(adress);
+            // Wait for the site to load and then open up the "Probenliste" (the table with all the samples (but not the actual values))
+            await page.evaluate(() => {
+                pfad(this.adress);
+                BlockLaden("probenliste", "#main");
+                // expose function to convert header and row data to JSON
+                window.convertToJSON = function (rows, header, clickValues) {
+                    for (let i = 0; i < rows.length; i++) {
+                        let splittedRows = (rows[i].split("\t"));
+                        let objectString = '{';
+                        for (let j = 0; j < header.length; j++) {
+                            //filter for invalid expressions in JSON Data
+                            splittedRows[j] = splittedRows[j].trim();
+                            if (splittedRows[j].includes('"') || splittedRows[j].includes("\\")) {
+                                splittedRows[j] = splittedRows[j].split('"').join();
+                                splittedRows[j] = splittedRows[j].split("\\").join();
+                            }
+                            //replace headername and set onClick function as value
+                            if (header[j] === "onClick") {
+                                splittedRows[j] = clickValues[i].attributes[2].nodeValue;
+                            }
+                            objectString += `"${header[j]}": "${splittedRows[j]}"`;
+                            if (j !== header.length - 1) {
+                                objectString += ', '
+                            } else {
+                                objectString += '}'
+                            }
                         }
-                        //replace headername and set onClick function as value
-                        if (header[j] === "onClick") {
-                            splittedRows[j] = clickValues[i].attributes[2].nodeValue;
-                        }
-                        objectString += `"${header[j]}": "${splittedRows[j]}"`;
-                        if (j !== header.length - 1) {
-                            objectString += ', '
-                        } else {
-                            objectString += '}'
-                        }
+                        rows[i] = JSON.parse(objectString);
                     }
-                    rows[i] = JSON.parse(objectString);
+                    return rows;
                 }
-                return rows;
-            }
-        });
-        // Wait for the list of samples to load
-        await page.waitForSelector("#probenliste_wrapper", { visible: true }).then(async () => {
-            const allSamples = await page.evaluate(async () => {
-                //create JSON Object form headers and rows of the datatable
-                let head = Array.from(document.querySelectorAll('#probenliste thead tr th'));
-                head = head.map(td => td.innerText);
-                //there is some wierd character in this header and i dont know how to remove it yet, so i just hardcoded it, because it shouldnt change anyway
-                head[5] = "Laufende Nummer der Probe";
-                let rows = Array.from(document.querySelectorAll('#probenliste tbody tr'));
-                rows = rows.map(td => td.innerText)
-                //get spans with onclick function to open correct detail list
-                let clickValues = Array.from(document.querySelectorAll('#probenliste tbody tr td span[title="Messwerte"]'));
-                //Change header name cause it stores the onClick value
-                head[2] = "onClick";
-                //covert data to JSON to be able to use it for filtering 
-                return await convertToJSON(rows, head, clickValues);
-            })
-            //write CSV containing the info of all measurement points
-            await writeCSV(measurementPath + `/measurementsInfo.csv`, headerAll, allSamples)
-            //filter Samples for period
-            const filteredSamples = await allSamples.filter(sample => filterSamples(from, until, sample));
-            for (let i = 0; i < filteredSamples.length; i++) {
-                //execute onClick function for filterd Values
-                await page.evaluate((filteredSamples, i) => {
-                    eval(filteredSamples[i].onClick);
-                }, filteredSamples, i);
-                // Wait for measurementdata to load
-                await page.waitForSelector(".ui-dialog .dataTables_scrollBody .display.dataTable tbody", { visible: true });
-                const csvData = await page.evaluate(async (header) => {
-                    let rows = Array.from(document.querySelectorAll('.ui-dialog .dataTables_scrollBody .display.dataTable tbody tr'));
+            });
+            // Wait for the list of samples to load
+            await page.waitForSelector("#probenliste_wrapper", { visible: true }).then(async () => {
+                const allSamples = await page.evaluate(async () => {
+                    //create JSON Object form headers and rows of the datatable
+                    let head = Array.from(document.querySelectorAll('#probenliste thead tr th'));
+                    head = head.map(td => td.innerText);
+                    //there is some wierd character in this header and i dont know how to remove it yet, so i just hardcoded it, because it shouldnt change anyway
+                    head[5] = "Laufende Nummer der Probe";
+                    let rows = Array.from(document.querySelectorAll('#probenliste tbody tr'));
                     rows = rows.map(td => td.innerText)
-                    return await convertToJSON(rows, header);
-                }, header);
-                //write CSV for single measurement
-                await writeCSV(measurementPath + `/messdaten/${filteredSamples[i]['Datum der Probenahme']}.csv`, header, csvData)
-                //close popup so new one can be opened
-                await page.keyboard.press('Escape');
-            }
-        });
-    } catch (e) {
+                    //get spans with onclick function to open correct detail list
+                    let clickValues = Array.from(document.querySelectorAll('#probenliste tbody tr td span[title="Messwerte"]'));
+                    //Change header name cause it stores the onClick value
+                    head[2] = "onClick";
+                    //covert data to JSON to be able to use it for filtering 
+                    return await convertToJSON(rows, head, clickValues);
+                })
+                //write CSV containing the info of all measurement points
+                await writeCSV(measurementPath + `/measurementsInfo.csv`, headerAll, allSamples)
+                //filter Samples for period
+                const filteredSamples = await allSamples.filter(sample => filterSamples(from, until, sample));
+                for (let i = 0; i < filteredSamples.length; i++) {
+                    //execute onClick function for filterd Values
+                    await page.evaluate((filteredSamples, i) => {
+                        eval(filteredSamples[i].onClick);
+                    }, filteredSamples, i);
+                    // Wait for measurementdata to load
+                    await page.waitForSelector(".ui-dialog .dataTables_scrollBody .display.dataTable tbody", { visible: true });
+                    const csvData = await page.evaluate(async (header) => {
+                        let rows = Array.from(document.querySelectorAll('.ui-dialog .dataTables_scrollBody .display.dataTable tbody tr'));
+                        rows = rows.map(td => td.innerText)
+                        return await convertToJSON(rows, header);
+                    }, header);
+                    //write CSV for single measurement
+                    await writeCSV(measurementPath + `/messdaten/${filteredSamples[i]['Datum der Probenahme']}.csv`, header, csvData)
+                    //close popup so new one can be opened
+                    await page.keyboard.press('Escape');
+                }
+            });
+        } catch (e) {
+            await browser.close();
+            index--;
+            spinners.fail(`spinner-${id}`, { text: 'Error: ' + e })
+        }
+        //close browser when finished
         await browser.close();
-        spinners.fail(`spinner-${id}`, { text: 'Error: ' + e })
-    }
-    //close browser when finished
-    await browser.close();
-    spinners.succeed(`spinner-${id}`, { text: `Measurement point ${id} Done!`});
+        index--;
+        spinners.succeed(`spinner-${id}`, { text: `Measurement point ${id} Done!` });
+    });
 }
 
 //parse dates and check if sample is in range
@@ -165,5 +175,13 @@ async function writeCSV(path, header, data) {
     })
     await csvWriter.writeRecords(data);
 }
+
+function timeout() {
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve();
+        }, 100);
+    });
+};
 
 export { createCSVs };
